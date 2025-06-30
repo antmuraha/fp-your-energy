bd64940bc484d8027737016feb31cf5d989206cc
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-06-30 17:19:48 +0300

    style: refactor renderFilterList function for improved readability and maintainability

--- src/js/renders/render_filter_list.js
+++ src/js/renders/render_filter_list.js
@@ -1,5 +1,8 @@
 import renderCardFilter from './render_card_filter';
 
+// TODO: Refactoring will be needed
+const pageSize = 12;
+
 function renderFilterList(data, selectedFilter) {
   const filterList = document.querySelector('.exercises-filters-list');
   if (selectedFilter) {
@@ -11,20 +14,21 @@ function renderFilterList(data, selectedFilter) {
   data.forEach((item, idx) => {
     const { filter, imgURL, name } = item;
     if (filterList.children[idx]) {
+      filterList.children[idx].classList.remove('hidden');
       renderCardFilter(filterList.children[idx], {
         filter: name,
         image: imgURL,
         group: filter,
       });
-    } else {
-      const filterItem = document.createElement('div');
-      filterItem.setAttribute('src', imgURL);
-      filterItem.setAttribute('group', filter.group);
-      filterItem.setAttribute('filter', filter.filter);
-      filterItem.setAttribute('image', filter.image);
-      filterList.appendChild(filterItem);
     }
   });
+
+  if (data.length < pageSize) {
+    const emptyCount = pageSize - data.length;
+    for (let i = 0; i < emptyCount; i++) {
+      filterList?.children[i + data.length]?.classList.add('hidden');
+    }
+  }
 }
 
 export default renderFilterList;
