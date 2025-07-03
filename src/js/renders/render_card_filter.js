b396f2a711a95507d2ceb06f210ec872b27dd7f3
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2025-07-04 00:07:40 +0200

    fix exercise card

--- src/js/renders/render_card_filter.js
+++ src/js/renders/render_card_filter.js
@@ -9,19 +9,27 @@ function renderCardFilter(element, { group, filter, image }) {
 
   if (element) {
     element.setAttribute('style', backgroundTemplate);
-    element.querySelector('h3').textContent = filter;
+    element.querySelector('h3').textContent = capitalizeFirstLetter(filter);
     element.querySelector('h3').setAttribute('data-filter', filter);
     element.querySelector('div').textContent = group;
     element.querySelector('div').setAttribute('data-group', group);
   } else {
     const newElement = template.cloneNode(true);
     newElement.setAttribute('style', backgroundTemplate);
-    newElement.querySelector('h3').textContent = filter;
+    newElement.querySelector('h3').textContent = capitalizeFirstLetter(filter);
     newElement.querySelector('h3').setAttribute('data-filter', filter);
     newElement.querySelector('div').textContent = group;
     newElement.querySelector('div').setAttribute('data-group', group);
 
     return newElement;
+  }
+}
+
+function capitalizeFirstLetter(string) {
+  if (typeof string === 'string') {
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+  } else {
+    return string;
   }
 }
 
